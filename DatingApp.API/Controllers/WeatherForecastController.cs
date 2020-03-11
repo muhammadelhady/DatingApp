463f1fe352e0a3c32b48800cba124f10557e5d34
muhammadelhady <muhammad.elhady@outlook.com>
2020-03-11 17:27:29 +0200

    initial commit

---
+++ DatingApp.API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DatingApp.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class Values : ControllerBase
+    {
+        private readonly DataContext _context; 
+        public Values(DataContext context)
+        {
+            _context=context;
+        }
+
+     
+
+        [HttpGet]
+        public ActionResult GetValues()
+        {
+           var Values = _context.Values.ToList();
+           return Ok(Values);
+        }
+
+
+
+         [HttpGet("{id}")]
+        public ActionResult GetValues(int id)
+        {
+           var Values = _context.Values.FirstOrDefault(x=>x.Id==id);
+           return Ok(Values);
+        }
+    }
+}
---
+++ DatingApp.API/models/values.cs
@@ -0,0 +1,8 @@
+namespace DatingApp.API.models
+{
+    public class values
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
